e0af57f1ecc97f4c6c94a3672faf8ee4b5273b9a
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2024-12-18 21:07:44 +0100

    fixed font error

--- src/client-app/app/layout.tsx
+++ src/client-app/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${roboto} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto.className} antialiased`}
       >
         {children}
       </body>
